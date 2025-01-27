1998bdf89f87936ea139810114b0acb29ee6378e
Samyak0-0 <146316899+Samyak0-0@users.noreply.github.com>
2025-01-27 16:41:21 +0545

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,111 +20,111 @@ app.get('/test', (req,res) => {
 	res.send("Prasiddha noob");
 })
 
-app.get('/home/search', (req, res) => {
-	const searchQuery = req.query.item;
-	const basePath = path.join(__dirname, '../resources/');
+// app.get('/home/search', (req, res) => {
+// 	const searchQuery = req.query.item;
+// 	const basePath = path.join(__dirname, '../resources/');
   
-	console.log('User searched for:', searchQuery);
+// 	console.log('User searched for:', searchQuery);
   
-	if (!searchQuery) {
-	  return res.send('Please provide a search query.');
-	}
+// 	if (!searchQuery) {
+// 	  return res.send('Please provide a search query.');
+// 	}
   
-	const findTarget = (dir, query) => {
-	  const items = fs.readdirSync(dir);
+// 	const findTarget = (dir, query) => {
+// 	  const items = fs.readdirSync(dir);
   
-	  for (const item of items) {
-		const fullPath = path.join(dir, item);
-		const isDirectory = fs.lstatSync(fullPath).isDirectory();
+// 	  for (const item of items) {
+// 		const fullPath = path.join(dir, item);
+// 		const isDirectory = fs.lstatSync(fullPath).isDirectory();
   
-		if (item.toLowerCase() === query.toLowerCase()) {
-		  return fullPath
-		}
+// 		if (item.toLowerCase() === query.toLowerCase()) {
+// 		  return fullPath
+// 		}
   
-		// recursion
-		if (isDirectory) {
-		  const match = findTarget(fullPath, query);
-		  if (match) return match;
-		}
-	  }
+// 		// recursion
+// 		if (isDirectory) {
+// 		  const match = findTarget(fullPath, query);
+// 		  if (match) return match;
+// 		}
+// 	  }
   
-	  return null;
-	};
+// 	  return null;
+// 	};
   
-	const match = findTarget(basePath, searchQuery);
+// 	const match = findTarget(basePath, searchQuery);
   
-	if (match) {
-	  const relativePath = path.relative(basePath, match).replace(/\\/g, '/');
-	  return res.redirect(`/list.html?semester=${relativePath}`);
-	} else {
-	  return res.status(404).send('Directory not found.');
-	}
-  });
+// 	if (match) {
+// 	  const relativePath = path.relative(basePath, match).replace(/\\/g, '/');
+// 	  return res.redirect(`/list.html?semester=${relativePath}`);
+// 	} else {
+// 	  return res.status(404).send('Directory not found.');
+// 	}
+//   });
 
 // for opening files
-app.get('/openfile/:department/:semester/:subject/*', (req, res) => {
-    const department = req.params.department;
-    const semester = req.params.semester;
-    const subject = req.params.subject;
-    const item = req.params[0];
+// app.get('/openfile/:department/:semester/:subject/*', (req, res) => {
+//     const department = req.params.department;
+//     const semester = req.params.semester;
+//     const subject = req.params.subject;
+//     const item = req.params[0];
 
-    const pdfPath = path.join(__dirname, `../resources/${department}/${semester}/${subject}/${decodeURIComponent(item)}`);
-    console.log(`Requested path: ${pdfPath}`);
+//     const pdfPath = path.join(__dirname, `../resources/${department}/${semester}/${subject}/${decodeURIComponent(item)}`);
+//     console.log(`Requested path: ${pdfPath}`);
 
-    fs.stat(pdfPath, (err, stats) => {
-        if (err) {
-            console.error('Path not found:', pdfPath);
-            return res.status(404).send('File or directory not found.');
-        }
+//     fs.stat(pdfPath, (err, stats) => {
+//         if (err) {
+//             console.error('Path not found:', pdfPath);
+//             return res.status(404).send('File or directory not found.');
+//         }
 
-        if (stats.isDirectory()) {
-            fs.readdir(pdfPath, (dirErr, files) => {
-                if (dirErr) {
-                    console.error('Error reading directory:', dirErr.message);
-                    return res.status(500).send('Error reading directory.');
-                }
-                res.json({
-                    type: 'directory',
-                    contents: files,
-                });
-            });
-        } else if (stats.isFile()) {
-            const pdfStream = fs.createReadStream(pdfPath);
-            res.setHeader('Content-Type', 'application/pdf');
-            pdfStream.pipe(res);
+//         if (stats.isDirectory()) {
+//             fs.readdir(pdfPath, (dirErr, files) => {
+//                 if (dirErr) {
+//                     console.error('Error reading directory:', dirErr.message);
+//                     return res.status(500).send('Error reading directory.');
+//                 }
+//                 res.json({
+//                     type: 'directory',
+//                     contents: files,
+//                 });
+//             });
+//         } else if (stats.isFile()) {
+//             const pdfStream = fs.createReadStream(pdfPath);
+//             res.setHeader('Content-Type', 'application/pdf');
+//             pdfStream.pipe(res);
 
-            pdfStream.on('error', (streamErr) => {
-                console.error('Error reading file:', streamErr.message);
-                res.status(500).send('Error reading file.');
-            });
-        } else {
-            res.status(400).send('Unsupported file type.');
-        }
-    });
-});
+//             pdfStream.on('error', (streamErr) => {
+//                 console.error('Error reading file:', streamErr.message);
+//                 res.status(500).send('Error reading file.');
+//             });
+//         } else {
+//             res.status(400).send('Unsupported file type.');
+//         }
+//     });
+// });
 
 // for list.js
-app.get('/resources/:department/:semester*', (req, res) => {
-	const department = req.params.department;
-    const semester = req.params.semester;
-    const dirPath = req.params[0] || '';
+// app.get('/resources/:department/:semester*', (req, res) => {
+// 	const department = req.params.department;
+//     const semester = req.params.semester;
+//     const dirPath = req.params[0] || '';
 	
-	const resourcesPath = path.join(__dirname, `../resources/${department}/`);
-    const fullPath = path.join(resourcesPath, semester, dirPath);
+// 	const resourcesPath = path.join(__dirname, `../resources/${department}/`);
+//     const fullPath = path.join(resourcesPath, semester, dirPath);
 
-    fs.readdir(fullPath, { withFileTypes: true }, (err, files) => {
-        if (err) {
-            return res.status(500).json({ error: 'Unable to read directory' });
-        }
+//     fs.readdir(fullPath, { withFileTypes: true }, (err, files) => {
+//         if (err) {
+//             return res.status(500).json({ error: 'Unable to read directory' });
+//         }
 
-        const filesList = files.map(file => ({
-            name: file.name,
-            isDirectory: file.isDirectory()
-        }));
+//         const filesList = files.map(file => ({
+//             name: file.name,
+//             isDirectory: file.isDirectory()
+//         }));
 
-        res.json(filesList);
-    });
-});
+//         res.json(filesList);
+//     });
+// });
 
 // Start the Express server
 app.listen(6969, "0.0.0.0", () => {
